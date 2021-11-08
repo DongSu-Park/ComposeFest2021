c90aa6d14920861d3861a5eb77252352d52d9ce3
박동수 <floreerin@gmail.com>
2021-11-08 23:17:53 +0900

    Week1 - 13. Finishing touches! 진행

--- week 1-Jetpack Compose basics/Week1_BasicsCodelab/app/src/main/java/flore/dspark/week1_basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1_BasicsCodelab/app/src/main/java/flore/dspark/week1_basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -210,7 +215,8 @@ private fun GreetingLazyColumn(names: List<String> = List(1000) {"$it"}){
     // 리사이클러 뷰와 비슷한 역할의 리스트 형식의 뷰를 만듬.
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
         items(items = names){
-            name -> GreetingButton(name = name)
+//            name -> GreetingButton(name = name)
+            name -> GreetingCard(name = name)
         }
     }
 }
@@ -236,5 +242,53 @@ fun LazyColumnPreview(){
 fun DefaultPreviewNormal(){
     Week1_BasicsCodelabTheme {
         GreetingLazyColumn()
+    }
+}
+
+/** 최종 UI 화면 */
+@Composable
+private fun GreetingCard(name : String){
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name : String){
+    var expanded by remember { mutableStateOf(false)}
+    
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)) {
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
+            
+                if (expanded){
+                    Text(text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4),)
+                }
+            }
+        IconButton(onClick = {expanded = !expanded}) {
+            Icon(imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded){
+                    stringResource(id = R.string.show_less)
+                } else{
+                    stringResource(id = R.string.show_more)
+                }
+            )
+        }
     }
 }
