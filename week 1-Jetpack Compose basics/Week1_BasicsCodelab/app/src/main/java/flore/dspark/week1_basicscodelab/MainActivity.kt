1b9cb80313a9ae454c7c634411efa72035f2859d
박동수 <floreerin@gmail.com>
2021-11-08 21:30:57 +0900

    Week1 - 6. Creating columns and rows 진행

--- week 1-Jetpack Compose basics/Week1_BasicsCodelab/app/src/main/java/flore/dspark/week1_basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1_BasicsCodelab/app/src/main/java/flore/dspark/week1_basicscodelab/MainActivity.kt
@@ -3,8 +3,12 @@ package flore.dspark.week1_basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -42,11 +46,85 @@ fun Greeting(name: String) {
     }
 }
 
+@Composable
+private fun MyAppRaw(names : List<String> = listOf("World", "Compose")){
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names){
+            GreetingRaw(name = name)
+        }
+    }
+}
+
+@Composable
+private fun GreetingRaw(name : String){
+    // columns and rows
+    // Surface의 영역에 Modifier로 패딩처리
+    // Column의 영역에 fillMaxWith()로 수평부분을 꽉차게 처리
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = name)
+        }
+    }
+}
+
+@Composable
+private fun MyAppButton(names : List<String> = listOf("World", "Compose")){
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names){
+            GreetingButton(name = name)
+        }
+    }
+}
+
+@Composable
+private fun GreetingButton(name : String){
+    // Surface -> Raw -> Column - OutlinedButton 순의 부모, 자식 레이아웃 구성
+    // OutlinedButton 을 활용한 버튼 추가
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week1_BasicsCodelabTheme {
 //        Greeting("Android")
         Greeting(name = "Compose Codelab!!")
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreviewRaw(){
+    // 320dp의 넓이로 프리뷰
+    Week1_BasicsCodelabTheme {
+        MyAppRaw()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreviewButton(){
+    Week1_BasicsCodelabTheme {
+        MyAppButton()
     }
 }
